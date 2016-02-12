902eabdf35eb7fff1f363dbcc36f00fce9aa1332
Jerry Chee <jchee@uchicago.edu>
2016-02-12 11:45:35 -0600

    keyword search

---
+++ tf-idf/keyword_search/keyword_driver.py
@@ -0,0 +1,44 @@
+# Jerry Chee
+# drive to run tf-idf keyword analysis
+# -*- coding: utf-8 -*-
+from keyword_functions import *
+
+# =============================================================================
+# change the per system. 
+# the script will check in path_save to see if the data objects exist.
+# if not, the script will generate them and save to path_save
+path_arxiv = '/home/jerry/Data/Hopper_Project/ptm_data/arxiv_processed_trunc/'
+path_save = './test_data_objects/'
+
+concepts_pickle = "master-concepts.p"
+concepts_txt = "master-concepts"
+path_concepts_txt = "./"
+# =============================================================================
+
+
+# load or generate concepts list
+if (check_exist(concepts_pickle, path_save) == False):
+    print("generating concepts list")
+    concept_list(concepts_txt, path_concepts_txt, path_save)
+
+concepts = pickle.load(open(path_save + concepts_pickle, 'r'))
+
+# generate regex expressions
+re_concepts = gen_regex(concepts)
+
+# load or generate dicts needed to compute tf-idf
+if (check_exist("arxiv_sep_byfile.p", path_save) == False
+        | check_exist("arxiv_sep_byword.p", path_save) == False
+        | check_exist("arxiv_wordsindoc.p", path_save) == False):
+    print("computing data dictionaries")
+    data_process(path_arxiv, re_concepts, path_save)
+
+arxiv_sep_byfile = pickle.load(open(path_save + "arxiv_sep_byfile.p", 'r'))
+arxiv_sep_byword = pickle.load(open(path_save + "arxiv_sep_byword.p", 'r'))
+arxiv_wordsindoc = pickle.load(open(path_save + "arxiv_wordsindoc.p", 'r'))
+
+# generate tf-idf scores
+if (check_exist("tfidf_arxiv.p", path_save) == False):
+    print("computing tf-idf scores")
+    compute_tfidf(path_arxiv, path_save, arxiv_sep_byfile,
+            arxiv_sep_byword, arxiv_wordsindoc)
---
+++ tf-idf/keyword_search/keyword_functions.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Jerry Chee
+# functions for keyword search by tf-idf score
+import os
+import re
+import timeit
+import math
+import codecs
+import string
+import pickle
+from nltk.tokenize import RegexpTokenizer
+
+def check_exist(fname, path):
+    """ checks if fname exists in path"""
+    return fname in os.listdir(path)
+
+def concept_list(concept_fname, path_concepts, path_save):
+    """ reads in a txt file, one term per line.
+        outputs as python list in given path
+        as pickled object. unicode """
+    # open file pointer
+    f = codecs.open(path_concepts+concept_fname, 'r', "utf-8")
+    
+    # output list
+    concepts = []
+
+    # read in lines
+    for line in f.readlines():
+        #print(line)
+        concepts.append(line.lower().strip("\n"))
+
+    # from observation the concept lists all had ''
+    while ('' in concepts):
+        concepts.remove('')
+
+    # pickle 
+    pickle.dump(concepts, open(path_save+concept_fname+'.p', 'w'))
+
+
+def gen_regex(concepts):
+    """ reads in list (unicode), outputs dictionary
+        where key value is keyword in concept,
+        value is regex"""
+    # create output dict
+    re_concepts = {}
+
+    # create regex expression for every keyword in concepts
+    re_keyword = ur''
+    re_match = ur"(\\-)|(\\–)|(\\\s)"
+    re_replace = u"[-\s–]"
+    for keyword in concepts:
+        re_keyword = re.sub(re_match, re_replace, re.escape(keyword))
+        re_concepts[keyword] = re_keyword
+
+    return re_concepts
+
+
+def data_process(path_arxiv, re_concepts, path_save): 
+    """ pickles the 3 dictionaries needed to compute 
+        tf-idf scores. """
+    # dictionaries
+    arxiv_wordsindoc = {}
+    arxiv_sep_byfile = {}
+    arxiv_sep_byword = {}
+    
+    # misc and timing var
+    articles = os.listdir(path_arxiv)
+    num_r = 0
+    start = timeit.default_timer()
+    elapsed = 0
+    count = 0
+
+    # reused runtime var
+    fp = None
+    txt = u''
+    tokens = []
+    tokenizer = RegexpTokenizer(ur'\w+')
+
+    for f in articles:
+        # import text
+        fp = codecs.open(path_arxiv+f, 'r', "utf-8", errors="ignore")
+        txt = fp.read()
+        txt = txt.lower()
+
+        # tokenize the article
+        tokens = tokenizer.tokenize(txt)
+
+        # save the word count to dict
+        arxiv_wordsindoc[f] = len(tokens)
+
+        # create sub dictionary
+        arxiv_sep_byfile[f] = {}
+
+        # find word counts in article
+        for keyword, r in re_concepts.iteritems():
+            #print(r)
+            #print(txt)
+            num_r = len(re.findall(r, txt, re.UNICODE))
+
+            if (num_r > 0):
+                arxiv_sep_byfile[f][keyword] = num_r
+
+        if (count % 100 == 0):
+            elapsed = timeit.default_timer() - start
+            start = timeit.default_timer()
+            print("{} articles complete, {}s elapsed".format(count, elapsed))
+        count += 1
+
+    # now let's comput byword
+    for keyword in re_concepts:
+        arxiv_sep_byword[keyword] = 0
+    for f in arxiv_sep_byfile:
+        for keyword in arxiv_sep_byfile[f]:
+            arxiv_sep_byword[keyword] += 1
+
+
+    # let's pickle them all
+    pickle.dump(arxiv_sep_byfile, open(path_save+"arxiv_sep_byfile.p", 'w'))
+    pickle.dump(arxiv_sep_byword, open(path_save+"arxiv_sep_byword.p", 'w'))
+    pickle.dump(arxiv_wordsindoc, open(path_save+"arxiv_wordsindoc.p", 'w'))
+
+
+# tf-idf functions. specific to arxiv only
+def tf(keyword, doc, corpus_byfile, corpus_wordsindoc):
+    """ computes term frequency"""
+    # must first check if word in document
+    if (keyword in corpus_byfile[doc]):
+        return (corpus_byfile[doc][keyword] / corpus_wordsindoc[doc])
+    else:
+        return 0
+
+def idf(keyword, corpus_byword, len_corpus):
+    """ computes inverse doc frequency """
+    return math.log(len_corpus / (1 + corpus_byword[keyword]))
+
+def tfidf(path, keyword, doc, corpus_byfile, corpus_byword, 
+        corpus_wordsindoc):
+    """ calls to tf() and idf(), multiplies"""
+    len_corpus = len(os.listdir(path))
+    return tf(keyword, doc, corpus_byfile, 
+            corpus_wordsindoc) * idf(keyword, corpus_byword, len_corpus)
+
+def compute_tfidf(corpus_path, path_save, corpus_byfile, 
+        corpus_byword, corpus_wordsindoc):
+    """ computes tfidf scores for all keywords, all articles"""
+    # output dict
+    tfidf_arxiv = {}
+
+    score = 0
+    for f in corpus_byfile:
+        # init sub dictionary
+        tfidf_arxiv[f] = {}
+
+        for keyword in corpus_byfile[f]:
+            score = tfidf(corpus_path, keyword, f, corpus_byfile, 
+                    corpus_byword, corpus_wordsindoc)
+
+            # only save if score is meaningful
+            if (score > 0):
+                tfidf_arxiv[f][keyword] = score
+
+    # pickle
+    pickle.dump(tfidf_arxiv, open(path_save+"tfidf_arxiv.p", 'w'))
+
